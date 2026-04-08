a6428ccb6eddbfd70880ec7173d27d96ed87f959
JULIO CESAR VALVERDE GASPARETO <julio_gaspareto@estudante.sesisenai.org.br>
2026-04-07 22:13:55 -0300

    fazendo front com o professor, fizemos a pagina de login

---
+++ clinica/src/components/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <div>
+        <div className='flex w-1/4 bg-black-1000'>
+        dadsadada
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm
